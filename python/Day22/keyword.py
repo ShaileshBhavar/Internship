116b18ad7f3226c26cd0320bbc25e65a8a877894
Shailesh Bhavar <shaileshbhavar521@gmail.com>
2026-01-31 12:56:17 +0530

    updated

---
+++ python/Day22/keyword.py
@@ -0,0 +1,21 @@
+# if
+# while
+# for
+# function
+
+name="a2z";
+
+
+
+""" variable-(storage) """
+# var name=value
+# name=value datatype-auto
+# rules
+# my name=
+# 12abc=
+# ab-cd=
+
+a=100;
+b=100;
+c=a+b;
+print(c);
